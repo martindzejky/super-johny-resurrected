ed6c379057b1ebe299c0e7ff5d10d1a66b1893a8
Martin Jakubik <martin@jakubik.info>
2017-01-07 20:13:19 +0100

    Add and load a Tiled level

---
+++ Assets/Scripts/DebugController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+
+/**
+ * Allows the player to quickly restart and quit the game.
+ */
+public class DebugController : MonoBehaviour {
+
+    public void Update() {
+        if (Input.GetButtonDown("Restart")) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        if (Input.GetButtonDown("Cancel")) {
+            #if UNITY_EDITOR
+                EditorApplication.isPlaying = false;
+            #else
+                Application.Quit();
+            #endif
+        }
+    }
+
+}
---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+
+/**
+ * Loads a TMX level and instantiates prefabs.
+ */
+public class LevelLoader : MonoBehaviour {
+
+    public string levelName = "TestArea";
+    public Transform container;
+    public GameObject[] prefabs;
+
+    public void Awake() {
+        // load the level from the Resources directory
+        var levelText = Resources.Load<TextAsset>("Levels/" + levelName);
+        var levelRows = levelText.text.Split('\n');
+
+        for (var y = 0; y < levelRows.Length - 1; y++) {
+            var levelRow = levelRows[y].Split(',');
+
+            for (var x = 0; x < levelRow.Length; x++) {
+                // get tile index
+                var tileIndex = int.Parse(levelRow[x]);
+
+                if (tileIndex < 0) {
+                    continue;
+                }
+
+                // instantiate and set up the tile
+                // mirror the tile position's Y coordinate
+                var tile = Instantiate(prefabs[tileIndex]);
+                tile.transform.SetParent(container);
+                tile.transform.position = new Vector3(x, -y, 0);
+            }
+        }
+    }
+
+}
