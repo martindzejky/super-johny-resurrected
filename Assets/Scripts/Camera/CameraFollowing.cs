0f6292420299946ea17542b72e06cd3d1aafa34d
Martin Jakubik <martin@jakubik.info>
2017-02-04 12:37:09 +0100

    Disable camera mouse control

--- Assets/Scripts/Camera/CameraFollowing.cs
+++ Assets/Scripts/Camera/CameraFollowing.cs
@@ -6,18 +6,18 @@
 /// </summary>
 public class CameraFollowing : MonoBehaviour {
 
+    private PlayerController player;
+
     public void LateUpdate() {
-        var target = FindObjectOfType<PlayerController>();
-        if (!target) {
-            return;
+        if (!player) {
+            player = FindObjectOfType<PlayerController>();
+
+            if (!player) {
+                return;
+            }
         }
 
-        var offset = new Vector2((Input.mousePosition.x - Screen.width / 2f) / Screen.width,
-            (Input.mousePosition.y - Screen.height / 2f) / Screen.height);
-        float maxOffset = 6f;
-
-        transform.position = new Vector3(target.transform.position.x + offset.x * maxOffset,
-            target.transform.position.y + offset.y * maxOffset, -10f);
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10f);
     }
 
 }
