686f8c84daf3d33cf7045bf8bdc25371e63abc38
Martin Jakubik <martin@jakubik.info>
2017-05-24 08:22:58 +0200

    Add player health UI
    Closes #19.

---
+++ Assets/Scripts/UI/LivesUI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LivesUI : MonoBehaviour {
+
+    public Image heartImage;
+    public Text livesText;
+
+    private PlayerController player;
+
+    private void LateUpdate() {
+        if (!player) {
+            player = FindObjectOfType<PlayerController>();
+
+            if (!player) {
+                heartImage.enabled = false;
+                livesText.enabled = false;
+                return;
+            }
+
+            heartImage.enabled = true;
+            livesText.enabled = true;
+        }
+
+        livesText.text = player.GetComponent<Mob>().lives.ToString();
+    }
+
+}
