a49fa9a8500be72ffbffa9cc730dc1733b10b171
Martin Jakubik <martin@jakubik.info>
2017-07-05 18:17:49 +0200

    Use advanced inspector for sprite registry

--- Assets/Scripts/Managers/SpriteRegistry.cs
+++ Assets/Scripts/Managers/SpriteRegistry.cs
@@ -1,11 +1,15 @@
-﻿using UnityEngine;
+﻿using AdvancedInspector;
+using UnityEngine;
 
 
 /// <summary>
 /// Contains lists for all sprites. Used mostly for selecting random sprites.
 /// </summary>
+[AdvancedInspector(true, false)]
 public class SpriteRegistry : MonoBehaviour {
 
+    [Help(HelpType.Info, "One of the bodies is selected when spawning a new mob")]
+    [Collection(Sortable = false, AlwaysExpanded = true)]
     public Sprite[] mobBodies;
 
 }
