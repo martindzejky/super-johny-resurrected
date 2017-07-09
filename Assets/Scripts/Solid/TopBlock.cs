aa7cc481fc4b209159b07ce089c05385875f0e70
Martin Jakubik <martin@jakubik.info>
2017-07-09 12:04:09 +0200

    Remove all existing solid blocks and keep only stone

---
+++ Assets/Scripts/Solid/TopBlock.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+/// <summary>
+/// Changes to a top block if no blocks of the same
+/// type are above.
+/// </summary>
+public class TopBlock : MonoBehaviour {
+
+    public GameObject topBlock;
+
+    private void Start() {
+        var solid = Physics2D.OverlapPoint(new Vector2(transform.position.x, transform.position.y + 1),
+            LayerMask.GetMask(Globals.solidLayerName));
+
+        if (!solid) {
+            Instantiate(topBlock, transform.localPosition, transform.localRotation, transform.parent);
+            Destroy(gameObject);
+        }
+    }
+
+}
